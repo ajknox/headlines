bcf70f3d59478b179c286dc6d854497c7fb194af
Andrew Knox <ajknox@gmail.com>
2017-02-20 13:49:11 -0500

    new content, new alignment rules for buttons, hover text

--- src/jsonify.py
+++ src/jsonify.py
@@ -1,8 +1,8 @@
 import csv
 import json
 
-infile_path = r'C:\Users\Husband Fish\Downloads\Read between the headlines - Sheet1(2).csv'
-outfile_path = r'C:\Users\Husband Fish\Documents\headlines\headlines.json'
+infile_path = r'C:\Users\Husband Fish\Downloads\Read between the headlines - Sheet1(3).csv'
+outfile_path = r'C:\Users\Husband Fish\repo\headlines\headlines.json'
 
 with open(infile_path, 'rb') as infile, open(outfile_path, 'w') as outfile:
     in_csv = csv.DictReader(infile)
